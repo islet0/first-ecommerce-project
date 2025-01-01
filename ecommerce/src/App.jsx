1d9fb23fbb176e8eb8e2978dee3515e866fc1039
islet0 <kh.farangiz2005@gmail.com>
2025-01-01 22:41:47 +0500

    Update App.jsx

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,9 +1,12 @@
 import Layout from "./components/layout/layout";
 import Error from "./components/errorpage"
+import About from "./components/aboutpage"
+
 
 function App() {
   return <Layout />
     <Error/>
+    <About/>
     ;
 }
 
